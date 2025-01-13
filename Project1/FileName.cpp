1d9d80c83053cb92ecb33dad56f5c454dfacc67d
Yanggeonmo454 <didrjsah05@naver.com>
2025-01-13 20:26:00 +0900

    in,out

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,1 +1,17 @@
-lll
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string val;
+
+    std::cout << "출력할 문자열을 입력하세요. 이 문자열은 5번 출력됩니다" << std::endl;
+    std::cin >> val;
+    std::cout << "입력된 값: ";
+    std::cout << val << std::endl;
+
+    for (int i = 1; i < 6; i++) {
+        std::cout << i << "회 출력! 입력한 문자열: " << val << std::endl;
+    }
+
+    return 0;
+}
